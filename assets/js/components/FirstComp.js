94aa8b9c98a9407cd6a9e785d25934f0dbb2771a
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-05-20 19:49:36 -0500

    first commit

---
+++ assets/js/components/FirstComp.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Nathaniel'
+		};
+	}
+	clickedBtn = () => {};
+	async test() {}
+	render() {
+		return (
+			<div className="home">
+				<div className="Aligner">
+					<div className="Aligner-item">
+						<h1>New React Course</h1>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout />, app);
