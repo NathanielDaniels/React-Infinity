5480c79a3a20ec5c150c04b5499f35c8c0d4bbd4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-03 12:37:25 -0500

    Object.assign() (Objects in State)

--- assets/js/components/Immutable/Immutable.js
+++ assets/js/components/Immutable/Immutable.js
@@ -5,19 +5,48 @@ export default class Immutable extends Component {
 	constructor() {
 		super();
 		this.state = {
-			status: 'manual',
-			currentNumber: 0
+			background: 'black',
+			user: {
+				name: 'Nate',
+				age: 23,
+				location: 'Chicago'
+			}
 		};
+		// console.log(this.state);
 	}
 
 	clickedBtn = () => {
-		console.log('clicked BTN');
+		console.log('button clicked');
+		const newUser = Object.assign({}, this.state.user, {
+			name: 'Bilbo'
+		});
+		console.log(this.state);
+		this.setState(
+			{
+				background: 'green',
+				user: newUser
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
+		if (this.state.user.name == 'Nate') {
+			console.log('Hello, Nate');
+		}
+	};
+
+	changeToActive = () => {
+		if (this.state.background == 'green') {
+			return 'active';
+		} else {
+			return '';
+		}
 	};
 
 	render() {
 		return (
 			<div id="Immutable-comp">
-				<div className="box" />
+				<div className={`box ${this.changeToActive()}`} />
 				<div className="button" onClick={this.clickedBtn}>
 					Press Me
 				</div>
