249ea65ae589c5ced6182011436ac659a5f85ea1
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-04 23:20:53 -0500

    Working on Immutable arrays

--- assets/js/components/Immutable/Immutable.js
+++ assets/js/components/Immutable/Immutable.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
+//Immutable Arrays
+let names = ['Cindy', 'Billy', 'Lisa', 'John'];
+let newNames = [...names, 'Ramsey'];
+// console.log(names);
+// console.log(newNames);
+
 // Class Compontent
 export default class Immutable extends Component {
 	constructor() {
 		super();
 		this.state = {
 			background: 'black',
+			names: ['Cindy', 'Billy', 'Lisa', 'John'],
+			teachers: ['Cam', 'Brad', 'Jenna'],
+			// both: [],
 			user: {
 				name: 'Nate',
-				age: 23,
+				age: 32,
 				location: 'Chicago',
 				grades: {
 					math: 'B',
@@ -18,38 +27,64 @@ export default class Immutable extends Component {
 				}
 			}
 		};
-		// console.log(this.state);
 	}
 
 	clickedBtn = () => {
-		console.log('button clicked');
-
 		//Object Spread (Only Use When Changing One Object)
 		const newGrade = {
 			...this.state.user.grades,
 			math: 'A+'
 		};
+		// Merge Two Arrays
+		const addEveryone = {
+			Everyone: [...this.state.names, ...this.state.teachers]
+		};
 		const newUser = Object.assign({}, this.state.user, {
 			name: 'Bilbo',
 			grades: newGrade
 		});
-		console.log(this.state.user.grades);
+
+		// // Add Name to End of Array
+		// const newNames = {
+		// 	names: [...this.state.names, 'Ramsey']
+		// };
+
+		// // Remove Name
+		// const newNames = {
+		// 	names: this.state.names.filter(name => name !== 'Billy')
+		// };
+
+		// // Merge Two Arrays
+		// const addEveryone = {
+		// 	both: [...this.state.names, ...this.state.teachers]
+		// };
+
+		console.log(this.state);
+
 		this.setState(
 			{
 				background: 'green',
-				user: newUser
+				user: newUser,
+				newNames,
+				both: addEveryone
 			},
 			() => {
 				console.log(this.state);
 			}
 		);
-		if (this.state.user.name == 'Nate') {
-			console.log('Hello, Nate');
-		}
-		if (this.state.user.grades.math == 'A') {
-			console.log('you are Math Smart');
+		// if (this.state.user.name == 'Nate') {
+		// 	console.log('Hello, Nate');
+		// }
+		// if (this.state.user.grades.math == 'A') {
+		// 	console.log('you are Math Smart');
+		// } else {
+		// 	console.log('Get Better At Math');
+		// }
+		console.log(this.state);
+		if (this.state.both[5] == 'Brad') {
+			console.log('its Brad alright');
 		} else {
-			console.log('Get Better At Math');
+			console.log('not brad');
 		}
 	};
 
