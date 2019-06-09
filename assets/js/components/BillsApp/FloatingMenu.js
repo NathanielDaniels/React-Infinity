1cb154481ab5d74a24832f853114f46b3d0642d6
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-09 01:01:29 -0500

    created scss from billsapp

---
+++ assets/js/components/BillsApp/FloatingMenu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// Class Compontent
+export default class FloatingMenu extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	clickedBtn = () => {};
+
+	changeToActive = () => {};
+
+	render() {
+		return (
+			<nav id="FloatingMenu">
+				<h1>Spend</h1>
+			</nav>
+		);
+	}
+}
