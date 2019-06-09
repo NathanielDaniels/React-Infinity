d26c42ebfa616b1e8a7268c3ae07f80e3cf48c3b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-09 00:50:53 -0500

    setting up footer

--- assets/js/components/BillsApp/AllBills.js
+++ assets/js/components/BillsApp/AllBills.js
@@ -19,6 +19,18 @@ export default class AllBills extends Component {
 						<div className="text">Total Amount:</div>
 						<div className="number">$874</div>
 					</div>
+
+					<ul className="bill-list">
+						<li className="bill">
+							<div className="company">
+								<div className="logo">
+									<img src="/img/billsapp/netflix.png" />
+								</div>
+								<div className="title">Netflix</div>
+							</div>
+							<div className="price">-$12.99</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 		);
