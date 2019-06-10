8474a252a8fd0ca55b0e8ce1a3c0019a25911e01
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-10 13:24:53 -0500

    added all menu buttons, now fixing resp. issues

--- assets/js/components/BillsApp/FloatingMenu.js
+++ assets/js/components/BillsApp/FloatingMenu.js
@@ -21,6 +21,31 @@ export default class FloatingMenu extends Component {
 						</div>
 						<div className="text">Spend</div>
 					</div>
+					<div className="link">
+						<div className="icon">
+							<i className="fas fa-piggy-bank" />
+						</div>
+						<div className="text">Save</div>
+					</div>
+					<div className="link">
+						<div className="icon">
+							<i className="fas fa-exchange-alt" />
+						</div>
+						<div className="text">Transfer</div>
+					</div>
+					<div className="link">
+						<div className="icon">
+							<i className="fas fa-cogs" />
+						</div>
+						<div className="text">Settings</div>
+					</div>
+					<div className="link">
+						<div className="add-button">
+							<div className="icon">
+								<i className="fas fa-plus" />
+							</div>
+						</div>
+					</div>
 				</nav>
 			</div>
 		);
