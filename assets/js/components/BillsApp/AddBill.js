a4e405cf310bd58ebd15623acc378071b227bb7c
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-12 17:20:57 -0500

    Working on popup form

--- assets/js/components/BillsApp/AddBill.js
+++ assets/js/components/BillsApp/AddBill.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BillsApp from './BillsApp';
+import { ECDH } from 'crypto';
 
 // Class Compontent
 export default class AllBills extends Component {
@@ -8,102 +9,27 @@ export default class AllBills extends Component {
 		this.state = {};
 	}
 
-	showAllBills = () => {
-		const bills = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-		return bills.map(bill => {
-			return (
-				<li className="bill" key={bill}>
-					<div className="company">
-						<div className="logo">
-							<img src="/img/billsapp/netflix.png" />
-						</div>
-						<div className="title">Netflix</div>
-					</div>
-					<div className="price">-$12.99</div>
-				</li>
-			);
-		});
-	};
-
 	render() {
 		return (
-			<div id="AllBills">
+			<div id="AddBill">
 				<div className="container">
-					<div className="total-bills">
-						<div className="text">Total Amount:</div>
-						<div className="number">$874</div>
-					</div>
+					<h2>Add Bill</h2>
+					<form>
+						<div className="form-group">
+							<label for="business_name">Business Name</label>
+							<input type="text" id="business_name" name="business_name" />
+						</div>
 
-					<ul className="bill-list">
-						{this.showAllBills()}
-						{/* <li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img src="/img/billsapp/netflix.png" />
-								</div>
-								<div className="title">Netflix</div>
-							</div>
-							<div className="price">-$12.99</div>
-						</li> */}
-						{/* <li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img
-										className="spotifyHulu"
-										src="/img/billsapp/hulu-spotify.png"
-									/>
-								</div>
-								<div className="title">Spotify/hulu</div>
-							</div>
-							<div className="price">-$9.99</div>
-						</li>
-						<li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img className="amazon" src="/img/billsapp/amazon.png" />
-								</div>
-								<div className="title">Amazon Prime</div>
-							</div>
-							<div className="price">-$12.99</div>
-						</li>
-						<li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img
-										className="codingPhase"
-										src="/img/billsapp/codingPhase.png"
-									/>
-								</div>
-								<div className="title">CodingPhase</div>
-							</div>
-							<div className="price">-$5.00</div>
-						</li>
-						<li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img
-										className="spotifyHulu"
-										src="/img/billsapp/laFitness.png"
-									/>
-								</div>
-								<div className="title">LA Fitness</div>
-							</div>
-							<div className="price">-$54.99</div>
-						</li>
-						<li className="bill">
-							<div className="company">
-								<div className="logo">
-									<img
-										className="spotifyHulu"
-										src="/img/billsapp/laFitness.png"
-									/>
-								</div>
-								<div className="title">LA Fitness</div>
-							</div>
-							<div className="price">-$54.99</div>
-						</li> */}
-					</ul>
+						<div className="form-group">
+							<label for="amount_due">Amount Due</label>
+							<input
+								type="number"
+								id="amount_due"
+								name="amount_due"
+								placeholder="$"
+							/>
+						</div>
+					</form>
 				</div>
 			</div>
 		);
--- assets/js/components/BillsApp/BillsApp.js
+++ assets/js/components/BillsApp/BillsApp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import AllBills from './AllBills';
 import FloatingMenu from './FloatingMenu';
+import AddBill from './AddBill';
 
 // Class Compontent
 export default class BillsApp extends Component {
@@ -19,6 +20,7 @@ export default class BillsApp extends Component {
 			<div id="BillsApp">
 				<Header />
 				<AllBills />
+				<AddBill />
 				<div className="content-background" />
 				<FloatingMenu />
 			</div>
